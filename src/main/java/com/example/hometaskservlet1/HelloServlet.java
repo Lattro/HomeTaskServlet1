878a160145ad7d1712a44f312168e542b4bfa93f
Lattro <artm.plyuto.98@mail.ru>
2023-11-23 20:01:28 +0300

    .\HomeTaskServlet2

--- src/main/java/com/example/hometaskservlet1/HelloServlet.java
+++ src/main/java/com/example/hometaskservlet1/HelloServlet.java
@@ -12,16 +12,28 @@ public class HelloServlet extends HttpServlet {
         message = "Hello World!";
     }
 
+    public String getVal(String value)
+    {
+        Double val;
+        try
+        {
+            val = Double.valueOf(value);
+        }
+        catch (Exception exception)
+        {
+            return exception.getMessage();
+        }
+        return "result value = "+String.valueOf(val*2);
+    }
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
-
-        // Hello
+        String id = request.getParameter("id");
         PrintWriter out = response.getWriter();
         out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
+        out.println("<h1> id = " + id + "</h1>");
+        out.println("<h1>"+ getVal(id)+"</h1>");
         out.println("</body></html>");
     }
-
     public void destroy() {
     }
 }
